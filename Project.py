780a4a35e524c5940e5860e242bf8d183d8a68e5
chong50 <70229773+chong50@users.noreply.github.com>
2023-04-22 00:07:32 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -86,6 +86,79 @@ async def scoring_endpoint(start_date: str, end_date: str):
     except Exception as e:
 
         return JSONResponse(content={"error": str(e)})
+    
+
+@app.get('/predicted/consumption/{start_date}/{end_date}')
+
+async def get_predicted(start_date: str, end_date: str):
+
+    try:
+
+        # Read data from Excel file
+        df2 = pd.read_excel("TimeSeriesForecasting.xlsx", usecols='C, D')
+        # Convert float values to string representations
+        df2 = df2.astype(str)
+
+        # start date and end date in datetime format
+        start_date = pd.to_datetime(
+            start_date, format="%Y-%m-%d-%H:%M")
+
+        end_date = pd.to_datetime(
+            end_date, format="%Y-%m-%d-%H:%M")
+        
+        df2['Time'] = pd.to_datetime(df2['Time'])
+
+        # filter by start and end date
+        df = df2[df2['Time'] >= start_date]
+        df = df[df['Time'] <= end_date]
+
+        df["Time"] = df["Time"].dt.strftime("%Y-%m-%d %H:%M")
+        
+        # Convert DataFrame to list of dictionaries
+        data = df.to_dict(orient='records')
+        return JSONResponse(content=data)
+
+    except Exception as e:
+
+        return JSONResponse(content={"error": str(e)})
+
+
+@app.get('/consumption/{start_date}/{end_date}')
+
+async def scoring_endpoint(start_date: str, end_date: str):
+
+    try:
+
+        # Read data from Excel file
+        df1 = pd.read_excel("TimeSeriesForecasting.xlsx", usecols='A, B')
+        # Convert float values to string representations
+        df1 = df1.astype(str)
+
+        # start date and end date in datetime format
+        start_date = pd.to_datetime(
+            start_date, format="%Y-%m-%d-%H:%M")
+
+        end_date = pd.to_datetime(
+            end_date, format="%Y-%m-%d-%H:%M")
+ 
+        df1['Time'] = pd.to_datetime(df1['Time'])
+
+ 
+
+        # filter by start and end date
+        df = df1[df1['Time'] >= start_date]
+        df = df[df['Time'] <= end_date]
+
+        df["Time"] = df["Time"].dt.strftime("%Y-%m-%d %H:%M")
+
+        # Convert DataFrame to list of dictionaries
+        data = df.to_dict(orient='records')
+        return JSONResponse(content=data)
+
+    except Exception as e:
+
+        return JSONResponse(content={"error": str(e)})
+    
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
